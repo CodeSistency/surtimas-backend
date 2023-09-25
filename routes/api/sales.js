8ee472098e72f4edebd81fd3c8e7fe7cb1d59b4e
Jose Perez <valero.jozee@gmail.com>
2023-09-25 11:14:08 -0400

    new update

--- routes/api/sales.js
+++ routes/api/sales.js
@@ -5,12 +5,12 @@ const ROLES_LIST = require('../../config/roles_list');
 const verifyRoles = require('../../middleware/verifyRoles');
 const upload = require("../../middleware/upload")
 
-router.route('/').post(verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Editor), salesController.newSale)
+router.route('/').post(verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Editor), salesController.newSale).get(salesController.getAllSales)
     // .put(verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Editor), productController.updateProduct)
     // .delete(verifyRoles(ROLES_LIST.Admin), productController.deleteProduct);
     
 
-// router.route('/:id')
+router.route('/:id').delete(salesController.deleteSale)
 //     .get(productController.getProduct)
 //     .delete(verifyRoles(ROLES_LIST.Admin),  productController.deleteProduct)
 //     .put(verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Editor), productController.updateProduct)
