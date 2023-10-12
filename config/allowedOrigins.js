4ce2b7792fbe4b7f1e35460187b69693df187828
CodeSistency <121405182+CodeSistency@users.noreply.github.com>
2023-10-12 17:08:53 -0400

    algo

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,7 +2,8 @@ const allowedOrigins = [
     'https://surtimas.vercel.app',
     'http://127.0.0.1:5500',
     'http://localhost:3500',
-    'http://localhost:3000'
+    'http://localhost:3000',
+    "https://www.tiendasurtymas.com"
 ];
 
 module.exports = allowedOrigins;
