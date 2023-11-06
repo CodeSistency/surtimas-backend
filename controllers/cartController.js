75a52a2d2e59695fb960c365066421e9aa1b6212
CodeSistency <121405182+CodeSistency@users.noreply.github.com>
2023-11-05 22:15:48 -0400

    algo

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -76,15 +76,12 @@ async function updateCartList(req, res) {
         for (const newItem of cart) {
             console.log(newItem.product)
             console.log(user)
-            const existingCartItem = user.cart.find(item => item.product === newItem.product);
+            const existingCartItem = user.cart.find(item => item.product.toString() === newItem.product);
             console.log(existingCartItem)
 
             if (existingCartItem) {
                 console.log("existe")
-                // Update the quantity of the existing cart item
-                // existingCartItem.quantity = newItem.quantity;
-                // existingCartItem.deseo = newItem.deseo;
-                // existingCartItem.sold = newItem.sold;
+                
                 existingCartItem.tallas = newItem.tallas;
 
 
